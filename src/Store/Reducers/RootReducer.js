38df6d53eae38cd21b79d24a89e8bd23a9eff896
Thilagaraj <tk.thilagaraj@gmail.com>
2018-10-16 15:22:09 +0530

    Update RootReducer.js

--- src/Store/Reducers/RootReducer.js
+++ src/Store/Reducers/RootReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import ChartReducer from './ChartReducer';
 import TradesReducer from './TradesReducer';
-import initialState from '../Data';
 
 const reducers = combineReducers({ chartState:ChartReducer,tradesState:TradesReducer});
 export default reducers;
